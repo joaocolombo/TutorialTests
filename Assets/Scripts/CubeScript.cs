053a04183a685fc847e6ffe893d3c6c50b7feb8d
Joao <joao.c@outlook.com>
2023-11-04 17:15:06 -0300

    fix everyting

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
+    public float InitialPointX;
+    public bool IsIncrementing;
+    public bool FirstFrameHappened;
+    public bool StartHappened;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        StartHappened = true;
+        InitialPointX = transform.position.x;
+        IsIncrementing = true;
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        FirstFrameHappened = true;
+        IncrementX();
+    }
+
+    public void IncrementX()
+    {
+        FirstFrameHappened = true;
+        transform.position += new Vector3(0.01f, 0, 0);
+
+        if (transform.position.x >= 2)
+        {
+            transform.position = new Vector3(InitialPointX, 0, 0);
+        }
     }
 }
--- Assets/Tests/EditMode/NewTestScript.cs
+++ Assets/Tests/EditMode/NewTestScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -8,18 +9,56 @@ public class NewTestScript
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses()
+    public void InitiateWithoutStartFirstFrame()
     {
-        // Use the Assert class to test conditions
+        var go = new GameObject();
+
+        CubeScript cubeScript = go.AddComponent<CubeScript>();
+
+        Assert.IsFalse(cubeScript.FirstFrameHappened);
+        Assert.IsFalse(cubeScript.StartHappened);
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    public IEnumerator InitiateWithStartFirstFrame()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        var go = new GameObject();
+
+        CubeScript cubeScript = go.AddComponent<CubeScript>();
+
+        cubeScript.Start();
+
+        yield return null;// one frame
+        cubeScript.Update();
+
+        yield return null;// two frame
+        cubeScript.Update();
+
+        Assert.True(cubeScript.FirstFrameHappened);
+        Assert.True(cubeScript.StartHappened);
+        Assert.AreEqual(0.02f,cubeScript.transform.position.x);
+
+    }
+
+
+
+    [UnityTest]
+    public IEnumerator Initiate12Times()
+    {
+        var go = new GameObject();
+
+        CubeScript cubeScript = go.AddComponent<CubeScript>();
+
+        cubeScript.Start();
+
+        foreach (var index in Enumerable.Range(0,201))
+        {
+            yield return null;// one frame
+            cubeScript.Update();
+        }
+        
+        Assert.True(cubeScript.FirstFrameHappened);
+        Assert.True(cubeScript.StartHappened);
+        Assert.AreEqual(0f, cubeScript.transform.position.x);
     }
 }
